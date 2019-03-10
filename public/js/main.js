8d5ecfe226e5767a2be0af331546ca29213bd00e
1653107 <1653107@student.hcmus.edu.vn>
2019-03-10 22:41:40 +0700

    Add page service detail

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+// Stick cover img in page service detail to top of its section
+$('div.container.service-detail div.img-cover').stick_in_parent();
+
+// Stick Guest review to top of its section
+$('div.row div.review-title').stick_in_parent();
