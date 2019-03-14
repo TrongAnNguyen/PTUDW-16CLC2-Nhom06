4fc0849950d6b3cb1188752dbc21111b533c0619
1653107 <1653107@student.hcmus.edu.vn>
2019-03-14 14:39:26 +0700

    Add see dates page

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
-// Stick cover img in page service detail to top of its section
+$(document).ready(function() {
+    // Stick cover img in page service detail to top of its section
 $('div.container.service-detail div.img-cover').stick_in_parent();
 
 // Stick Guest review to top of its section
 $('div.row div.review-title').stick_in_parent();
+
+$('section#see-dates div.container-calendar > div.col-lg-6 > div.calendar').stick_in_parent({parent  : 'section#see-dates div.header'});
+});
