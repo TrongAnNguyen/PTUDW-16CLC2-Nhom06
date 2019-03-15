945c8b0d40d82149a07e5f35d5ce2ae0fb68edda
1653107 <1653107@student.hcmus.edu.vn>
2019-03-15 21:40:15 +0700

    Add confirm booking page, update Service detail

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,4 @@ $('div.container.service-detail div.img-cover').stick_in_parent();
 
 // Stick Guest review to top of its section
 $('div.row div.review-title').stick_in_parent();
-
-$('section#see-dates div.container-calendar > div.col-lg-6 > div.calendar').stick_in_parent({parent  : 'section#see-dates div.header'});
 });
